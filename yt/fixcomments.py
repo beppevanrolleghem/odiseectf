74942b0e7015f891345ce4c2468fc53cc1050c9d
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2018-11-20 21:06:11 +0100

    fucking damnit

--- yt/fixcomments.py
+++ yt/fixcomments.py
@@ -9,6 +9,8 @@ with open('commets.txt', 'w') as outfile:
         try:
             if (item["commentText"]):
                 temp = item["commentText"]
-                outfile.write(temp.replace("\n", " ").replace("\r", " ")+"\n")
+                woordjes = temp.replace("\n", " ").replace("\r", " ").split(" ")
+                for w in woordjes:
+                    outfile.write(w+"\n")
         except KeyError:
                 print("wss een comment reply")
