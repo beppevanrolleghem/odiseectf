9941a77679aa79dce8f17ab3e24e257d2eeb7638
Beppe Vanrolleghem <beppe.vanrolleghem@gmail.com>
2018-11-20 20:55:26 +0100

    fucking damnit man

--- yt/fixcomments.py
+++ yt/fixcomments.py
@@ -3,11 +3,12 @@ import json
 
 with open('comments-AKLrKMz-avE.json', 'r') as infile:
     j = json.load(infile)
+with open('commets.txt', 'w') as outfile:
+    for item in j:
 
-for item in j:
-    try:
-        if (item["commentText"]):
-            temp = item["commentText"]
-            print(temp.replace("\n", " ").replace("\r", " "))
-    except KeyError:
-            print("wss een comment reply")
+        try:
+            if (item["commentText"]):
+                temp = item["commentText"]
+                outfile.write(temp.replace("\n", " ").replace("\r", " ")+"\n")
+        except KeyError:
+                print("wss een comment reply")
